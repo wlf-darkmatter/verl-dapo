b4b8778a03b1704e7b946cb7defe63624e34eb9e
jiaqiw09 <jiaqiw960714@gmail.com>
2025-06-30 23:43:53 +0800

    add scripts

--- run_qwen3_30b_dapo.sh
+++ run_qwen3_30b_dapo.sh
@@ -7,7 +7,7 @@ set -xeuo pipefail
 # huggingface-cli download deepseek-ai/DeepSeek-V3-0324 configuration_deepseek.py config.json
 
 project_name='DAPO'
-exp_name='DAPO-qwen3-235b-megatron'
+exp_name='DAPO-qwen3-30b-megatron'
 
 adv_estimator=grpo
 
@@ -37,8 +37,8 @@ train_prompt_mini_bsz=32  # mini_bsz * n >= micro_bsz * pp * dp
 
 NNODES=${NNODES:-32}
 
-MODEL_PATH="/Qwen/Qwen3-235B-A22B"
-MCORE_MODEL_PATH="/mcore/Qwen3-235B-A22B"
+MODEL_PATH="/Qwen/Qwen3-30B"
+MCORE_MODEL_PATH="/mcore/Qwen3-30B"
 RAY_DATA_HOME=${RAY_DATA_HOME:-"${HOME}/verl"}
 CKPTS_DIR=${CKPTS_DIR:-"${RAY_DATA_HOME}/ckpts/${project_name}/${exp_name}"}
 TRAIN_FILE="dapo-math-17k.parquet"
@@ -56,11 +56,11 @@ actor_ppo_max_token_len=$(((max_prompt_length + max_response_length) * 2))
 infer_ppo_max_token_len=$(((max_prompt_length + max_response_length) * 3))
 offload=True
 gen_tp=2
-gen_dp=64
+gen_dp=8
 train_tp=4
-train_ep=8
-train_pp=16
-train_cp=8
+train_ep=1
+train_pp=4
+train_cp=2
 
 python3 -m recipe.dapo.main_dapo \
     data.train_files="${TRAIN_FILE}" \
